cf475043e966be893015aa3ad5a99ecc8df4734b
Michael Aggerholm <92aggerholm@gmail.com>
2023-07-15 21:33:45 +0200

    Styling af navbar og tilføjelse af svg ikoner hertil

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,3 +1,54 @@
-<nav>
-    <h1>This is nav</h1>
+<nav class="menu">
+    
+    <img class="logo" src="{{asset('img/logo.svg')}}" alt="Home Logo">
+
+    <ul>
+    
+        <li>
+            <a href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="m21.743 12.331l-9-10c-.379-.422-1.107-.422-1.486 
+                    0l-9 10a.998.998 0 0 0-.17 1.076c.16.361.518.593.913.593h2v7a1 1 0 0 0 1 1h3a1 
+                    1 0 0 0 1-1v-4h4v4a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-7h2a.998.998 0 0 0 .743-1.669z"/>
+                </svg>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 36 36">
+                    <path fill="currentColor" d="M34 16.78a2.22 2.22 0 0 0-1.29-4l-9-.34a.23.23 0 0 
+                    1-.2-.15l-3.11-8.4a2.22 2.22 0 0 0-4.17 0l-3.1 8.43a.23.23 0 0 1-.2.15l-9 .34a2.22 
+                    2.22 0 0 0-1.29 4l7.06 5.55a.23.23 0 0 1 .08.24l-2.43 8.61a2.22 2.22 0 0 0 3.38 
+                    2.45l7.46-5a.22.22 0 0 1 .25 0l7.46 5a2.2 2.2 0 0 0 2.55 0a2.2 2.2 0 0 0 
+                    .83-2.4l-2.45-8.64a.22.22 0 0 1 .08-.24Z" class="clr-i-solid clr-i-solid-path-1"/>
+                    <path fill="none" d="M0 0h36v36H0z"/>
+                </svg>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <span class="info-count">3</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
+                    <mask id="ipSShopping0"><g fill="none"><path fill="#fff" d="M39 32H13L8 12h36l-5 20Z"/>
+                        <path stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" 
+                        d="M3 6h3.5L8 12m0 0l5 20h26l5-20H8Z"/><circle cx="13" cy="39" r="3" stroke="#fff" 
+                        stroke-linecap="round" stroke-linejoin="round" stroke-width="4"/><circle cx="39" 
+                        cy="39" r="3" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"/>
+                </g></mask><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipSShopping0)"/></svg>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256">
+                    <path fill="currentColor" d="M230.93 220a8 8 0 0 1-6.93 4H32a8 8 0 0 1-6.92-12c15.23-26.33 
+                    38.7-45.21 66.09-54.16a72 72 0 1 1 73.66 0c27.39 8.95 50.86 27.83 66.09 54.16a8 8 0 0 1 .01 8Z"/>
+                </svg>
+            </a>
+        </li>
+
+    </ul>
+
 </nav>
