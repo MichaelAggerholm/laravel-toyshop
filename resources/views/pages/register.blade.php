537500623817f55af232dbad2691dc97797c47f0
Michael Aggerholm <92aggerholm@gmail.com>
2023-07-15 22:02:49 +0200

    Styling af registration side og forbedredelse til login side

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts.master')
 @section('title', 'Register')
 @section('content')
-    <h1>Register Page</h1>
+    <section class="login-page">
+        <div class="login-form-box">
+            <div class="login-title">Register</div>
+            <div class="login-form">
+                <form action="" method="post">
+
+                    <div class="field">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" placeholder="John Doe" required>
+                    </div>
+                    
+                    <div class="field">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" placeholder="John@gmail.com" required>
+                    </div>
+
+                    <div class="field">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" placeholder="********" required>
+                    </div>
+
+                    <div class="field">
+                        <label for="password_confirmation">Confirm Password</label>
+                        <input type="password" name="password_confirmation" id="password_confirmation" placeholder="********" required>
+                    </div>
+
+                    <div class="field">
+                        <button type="submit" class="btn btn-primary btn-block">Register</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </section>
 @endsection
