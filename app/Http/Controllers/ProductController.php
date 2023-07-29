ee00163f06caaa5bd7ac8372b328def74b37da24
Michael Aggerholm <92aggerholm@gmail.com>
2023-07-29 22:09:41 +0200

    tilføjelse af produkter, kategorier og farver i productcontroller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,45 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Color;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    // adminpanel
-
     // Display products table
-    public function index()
-    {
-        return view('admin.pages.products.index');
+    public function index() {
+        $products = Product::all();
+
+        return view('admin.pages.products.index', ['products' => $products]);
     }
 
     // Create new product
-    public function create()
-    {
-        return view('admin.pages.products.create');
+    public function create() {
+        $categories = Category::all();
+        $colors = Color::all();
+
+        return view('admin.pages.products.create', ['categories' => $categories, 'colors' => $colors]);
     }
 
     // Store new product
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         return 'store';
     }
 
     // Edit product
-    public function edit($id)
-    {
+    public function edit($id) {
         return view('admin.pages.products.edit');
     }
 
     // Update product
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id) {
         return 'update';
     }
 
     // Delete product
-    public function destroy($id)
-    {
+    public function destroy($id) {
         return 'destroy';
     }
 }
