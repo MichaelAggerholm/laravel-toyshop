d064aaeeb0651451e50c6a292b14b9b194118802
Michael Aggerholm <92aggerholm@gmail.com>
2023-07-30 21:29:57 +0200

    Tilføjet registrer knap til loginside.

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -7,7 +7,7 @@
             <div class="login-form">
                 <form action="{{route('login')}}" method="post">
                     @csrf
-                    
+
                     <div class="field">
                         <label for="email">Email</label>
                         <!-- TODO: Gem gammel data ved fejlende registrering -->
@@ -28,6 +28,8 @@
                     <div class="field">
                         <button type="submit" class="btn btn-primary btn-block">Login</button>
                     </div>
+
+                    <a href="{{route('register')}}">Ny bruger? Registrer nu..</a>
 
                 </form>
             </div>
