59dcc468080bcfd18299e61719c139d7c1ca17b6
Michael Aggerholm <92aggerholm@gmail.com>
2023-07-30 21:30:55 +0200

    fjernet logud knap fra home og tilføjet til account side i stedet.

--- resources/views/pages/account.blade.php
+++ resources/views/pages/account.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.master')
 @section('title', 'account')
 @section('content')
-    <h1>Account Page</h1>
+    <div class="account-page">
+        <div class="container">
+            @auth
+                <form action="{{route('logout')}}" method="post">
+                    @csrf
+                    <button class="btn btn-primary">Logout</button>
+                </form>
+            @endauth
+        </div>
+    </div>
 @endsection
