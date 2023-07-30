685fdb099fb32c7e039c87a57375f1e4e8804865
Michael Aggerholm <92aggerholm@gmail.com>
2023-07-30 21:29:19 +0200

    Oprettet komponent igennem artisan make, samt tilføjet produkt visning med statiske værdier og sendt $products med ind i view fra productBox.php komponenten

---
+++ resources/views/components/product-box.blade.php
@@ -0,0 +1,13 @@
+<section class="product-box">
+    <div class="image">
+        <img src="{{asset('img/products/c1.png')}}" alt="">
+    </div>
+    <div class="product-title">{{$product->title}}</div>
+    <div class="color-plateletes">
+        <div class="color-platelete" style="background: red"></div>
+        <div class="color-platelete" style="background: blue"></div>
+        <div class="color-platelete" style="background: green"></div>
+    </div>
+    <div class="product-category">Chairs</div>
+    <div class="product-price">100 Dkk,-</div>
+</section>
