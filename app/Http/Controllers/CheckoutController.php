bb541753d451cf10983fd1d8c3db35a6bd4ca553
Michael Aggerholm <92aggerholm@gmail.com>
2023-09-12 19:46:40 +0200

    Når stripe anmodning er lavet og success, opret Order og Order items, gem til db, clear kurv, redirect med order object.

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
+use App\Models\Order;
 use Illuminate\Http\Request;
 use Stripe\Exception\ApiErrorException;
 use Stripe\PaymentIntent;
@@ -10,15 +11,23 @@ use Stripe\Stripe;
 
 class CheckoutController extends Controller
 {
-    public function stripeCheckout(Request $request)
-    {
+    public function stripeCheckout(Request $request) {
+
+        // Validate the request input
+        $request->validate([
+            'payment_method_id' => 'required',
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'address' => 'required|max:255',
+        ]);
+
+        // Set Stripe public key
         Stripe::setApiKey(getenv('STRIPE_SECRET_KEY'));
 
-        $intent = null;
         try {
             if ($request->payment_method_id) {
-                //Create the PaymentIntent
-                $intent = PaymentIntent::create([
+                //Create the PaymentIntent, this will create the order details to stripe
+                PaymentIntent::create([
                     'payment_method' => $request->payment_method_id,
                     'amount' => Cart::totalAmount() * 100,
                     'currency' => 'dkk',
@@ -27,22 +36,42 @@ class CheckoutController extends Controller
                     'return_url' => route('success'),
                 ]);
             }
-            if ($request->payment_intent_id) {
-                // Confirm the PaymentIntent to finalize payment after handling a required action
-                $intent = PaymentIntent::retrieve(
-                    $request->payment_intent_id
-                );
-                $intent->confirm();
-            }
         } catch (ApiErrorException $e) {
-            # Display error on client
+            // Handle error somewhat for now
             echo json_encode([
                 'error' => $e->getMessage()
             ]);
         }
 
         // Store order in database
+        $order = Order::create([
+            'user_id' => auth()->id(),
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'country' => $request->country,
+            'state' => $request->state, // TODO: Not using state
+            'city' => $request->city,
+            'zip' => $request->zipcode,
+            'stripe_id' => $request->payment_method_id,
+            'status' => 'pending',
+            'total' => Cart::totalAmount() * 100,
+        ]);
 
-         return redirect()->route('success');
+        // Store order items in database
+        foreach (session()->get('cart') as $item) {
+            $order->items()->create([
+                'product_id' => $item['product']['id'],
+                'color_id' => $item['color']['id'],
+                'quantity' => $item['quantity'],
+            ]);
+        }
+
+        // Everything is done, payment done, order and order items stored in database, so we clear the cart
+        session()->forget('cart');
+
+        // Redirect to success page
+         return redirect()->route('success', ['order' => $order]);
     }
 }
