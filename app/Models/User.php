f333c0f46e23602150a7fe81051b143c592e7adf
Michael Aggerholm <92aggerholm@gmail.com>
2023-09-16 09:39:52 +0200

    tilføjet ordrer og lidt brugerinfo til account page, med styling, stort set genbrugt fra kurv table.

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function user() {
+    public function orders() {
         return $this->hasMany(Order::class);
     }
 }
--- resources/views/pages/account.blade.php
+++ resources/views/pages/account.blade.php
@@ -3,12 +3,55 @@
 @section('content')
     <div class="account-page">
         <div class="container">
-            @auth
-                <form action="{{route('logout')}}" method="post">
-                    @csrf
-                    <button class="btn btn-primary">Logout</button>
-                </form>
-            @endauth
+            <section class="u-box">
+                <div class="user-info">
+                    <p class="user-name">
+                        {{auth()->user()->name}}
+                    </p>
+                    <p class="user-email">
+                        {{auth()->user()->email}}
+                    </p>
+                </div>
+                <div class="user-btn">
+                    <form action="{{route('logout')}}" method="post">
+                        @csrf
+                        <button class="btn btn-primary">Logout</button>
+                    </form>
+                </div>
+            </section>
+
+            <section class="orders-box">
+                <p class="orders-box-title">Orders</p>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Items</th>
+                            <th>Total</th>
+                            <th>Date</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @if(auth()->user()->orders && auth()->user()->orders->count())
+                        @foreach(auth()->user()->orders as $order)
+                            <tr>
+                                <td>{{$order->id}}</td>
+                                <td>{{$order->items->count()}}</td>
+                                <td>{{$order->total / 100}} Kr,-</td>
+                                <td>{{\Carbon\Carbon::parse($order->created_at)->format('d/m - Y')}}</td>
+                                <td>{{$order->status}}</td>
+                            </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            <td colspan="5" style="text-align:center">ingen ordrer fundet</td>
+                        </tr>
+                    @endif
+                    </tbody>
+                </table>
+            </section>
+
         </div>
     </div>
 @endsection
